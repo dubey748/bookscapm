7ee05ad0f10c66845c5809b29c2aa49f7b39f62c
Abhishek Dubey <abhishek.du@icloud.com>
2026-03-26 09:43:37 +0000

    Custom PUT DELETE CREATE

--- srv/catalogsrv.js
+++ srv/catalogsrv.js
@@ -1,5 +1,4 @@
 const cds = require('@sap/cds')
-const { SELECT } = require('@sap/cds/lib/ql/cds-ql')
 const { Books } = cds.entities
 
 module.exports = srv => {
@@ -9,12 +8,66 @@ module.exports = srv => {
         result = []
         results = await db.run([
             //For all data
-            // SELECT.from(Books)
+            SELECT.from(Books)
 
             //For specific data
-            SELECT.from(Books).where({ID:req.data.ID})
+            // SELECT.from(Books).where({ ID: req.data.ID })
 
         ])
         return results
+    })
+
+    // CREATE
+
+    srv.on('CREATE', 'Books', async (req, resp) => {
+
+        results = [];
+
+        results = await db.run(
+            INSERT.into(Books).entries(req.data)
+        )
+            .then((resolve, reject) => {
+                if (resolve)
+                    return req.data;
+                else
+                    return req.error(400, 'Failed to Create');
+            })
+            .catch(err => {
+                return req.error(500, 'Server down, Try again' + err.toString());
+            })
+
+        return results;
+
+    });
+
+    //UPDATE
+    srv.on('UPDATE', 'Books', async (req, resp) => {
+        console.log("CUSTOM UPDATE CALLED");
+        results = [];
+
+        results = await db.run([
+            UPDATE(Books).set(req.data).where({ ID: req.data.ID })
+        ])
+            .then((resolve, reject) => {
+                if (resolve)
+                    return req.data;
+                else
+                    return req.error(400, 'Failed to update');
+            })
+            .catch(err => {
+                req.error(500, 'Try after some time....');
+            })
+
+        return results;
+    });
+
+    srv.on('DELETE', 'Books', async (req, resp) => {
+        console.log("CUSTOM UPDATE CALLED");
+        results = [];
+
+        results = await db.run([
+            DELETE.from(Books).where({ ID: req.data.ID })
+        ])
+        return results;
     })
 }
